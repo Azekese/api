36c73cb0f21869ac9e97c158d5941baaf8c8de73
Azekese <92989988+Azekese@users.noreply.github.com>
2023-04-12 18:40:13 +0300

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -1,9 +1,18 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import datetime
-import calendar
+import argparse
 
-hostName = "localhost"
-serverPort = 8080
+
+parser = argparse.ArgumentParser(description="Simple web app",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--host", default="0.0.0.0")
+parser.add_argument("--port", type=int, default=8080)
+args = parser.parse_args()
+config = vars(args)
+
+hostName = config.get('host')
+serverPort = config.get('port')
+
 
 class MyServer(BaseHTTPRequestHandler):
 
@@ -14,17 +23,16 @@ class MyServer(BaseHTTPRequestHandler):
         if self.path == "/":
             self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
             self.wfile.write(bytes("<body>", "utf-8"))
-            self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+            self.wfile.write(bytes("<p>This is root page.</p>", "utf-8"))
             self.wfile.write(bytes("</body></html>", "utf-8"))
         elif self.path == "/hello":
             self.wfile.write(bytes("hello world!", "utf-8"))
         elif self.path == "/date":
-            now = datetime.datetime.today().strftime("%d/%m/%Y")
-            self.wfile.write(bytes("Current Date: " + str(now), "utf-8"))
+            self.wfile.write(bytes("Current Date: " + str(datetime.datetime.now().isoformat()), "utf-8"))
+
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
         webServer.serve_forever()
@@ -32,4 +40,3 @@ if __name__ == "__main__":
         pass
 
     webServer.server_close()
-    print("Server stopped.")
